280936e0e9b6f644db124113d7a7b64c81b0cee2
NIGGER <alanKYS@KYS.xyz>
2023-09-26 09:47:06 +0200

    i like your kodzik

--- Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -2,12 +2,61 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Napisz program wczytujący od użytkownika liczbę i wyświetlającą ją
+    int Liczba;
+
+    std::cout << "Dawaj liczbe draniu\n";
+    int numberFromUser;
+    std::cin >> numberFromUser;
+    std::cout << "Masz farta... " << numberFromUser << "\n";
+
+
+
 }
 
 /*
 Algorytm -skończony zbiór instrukcji, który rozwiązuje zadany problem.
 okresla kolejność wykonywania instrukcji.
 zapis algorytmu
+*rysunki
+* opis słowny
+* w punktach
+* schemat blokowy
+* kod źródłowy danego języka programowania
+* pseudokod
+* 
+* zmienna - pewien obszar pamięci operacyjnech, w której można w danej chwili przechować tylko jedną daną
 
+deklaracja zmiennej:
+typ_zmiennej nazwa_zmiennej
+
+typ zmiennej - wielkość obszaru pamięci, interpretacja ciągu bitów 
+
+int - 4 bajtowa liczba całkowita ze znakiem <-2 147 483 648, 2 147 483 647>
+short - 2 bajtowa liczba całokowita ze znakiem <-32 768, 32 767>
+long - to samo co int
+longlong - 8 bajtowa liczba ze znakiem <- 9 223 372 036 854 775 808, 9 223 372 036 854 775 807>
+
+unsigned - zmienna bez znaku <0, 2*max + 1>
+
+float - 4 bajtowa liczba rzeczywista - dokładność 6-7 cyfr po przecinku
+double - 8 bajtowa liczba rzeczywista - dokładność 15-16 cyfr po przecinku
+long double - 12 bajtowa liczba rzeczywista - dokładność 19-20 cyfr po przecinku
+
+Nazwa zmiennej - nazwa obszaru pamięci, identyfikator
+Warunki niezbędne:
+*Dozwolone znaki:
+    - alfabet angielski aA-zZ
+    - cyfry arabskie 0-9
+    - podłoga
+* pierwszym znakie nie może być cyfra
+* musi być unikalny w swoim zakresie widoczności
+* nie może to być słowo kluczowe (zarezerwowane) daniego języka
+
+warunki programistów:
+*nazwa zmiennej powinna oddawać charakter przechowywanych danych
+* po angielsku
 */
+
+
+
