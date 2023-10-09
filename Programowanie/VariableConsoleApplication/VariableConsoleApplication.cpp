e44b106cb6c5b619e99a2e0cfa68d86396aa077d
NIGGER <alanKYS@KYS.xyz>
2023-10-09 16:03:58 +0200

    dalsze zadanka

--- Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -14,99 +14,160 @@
 */
 void task1()
 {
-    std::cout << "Dawaj liczbe draniu\n";
-    int numberFromUser;
-    std::cin >> numberFromUser;
-    std::cout << "Masz farta... " << numberFromUser << "\n";
+	std::cout << "Dawaj liczbe draniu\n";
+	int numberFromUser;
+	std::cin >> numberFromUser;
+	std::cout << "Masz farta... " << numberFromUser << "\n";
 
 }
 
 void task2()
 {
-    short firstNumber;
-    short secondNumber;
+	short firstNumber;
+	short secondNumber;
 
-    std::cout << "Dewej mnie liczbe pierwsza: ";
-    std::cin >> firstNumber;
-    std::cout << "Dewej mnie liczbe druga: ";
-    std::cin >> secondNumber;
-    float srednia;
-    srednia = (firstNumber + secondNumber) / 2;
-    std::cout << "Srednia wynosi: " << srednia;
+	std::cout << "Dewej mnie liczbe pierwsza: ";
+	std::cin >> firstNumber;
+	std::cout << "Dewej mnie liczbe druga: ";
+	std::cin >> secondNumber;
+	float srednia;
+	srednia = (firstNumber + secondNumber) / 2;
+	std::cout << "Srednia wynosi: " << srednia;
 }
 
 void task3()
 {
-    /**.Program obliczający pole prostokąta.*/
-    int firstSide, secondSide, area ;
-    std::cout << "Podaj pierwszy bok prostokąta: ";
-    std::cin >> firstSide;
-    std::cout << "Podaj drugi bok prostokąta: ";
-    std::cin >> secondSide;
-    area = firstSide * secondSide;
-    std::cout << "Pole prostokąta wynosi " << area << " cm2";
+	/**.Program obliczający pole prostokąta.*/
+	int firstSide, secondSide, area;
+	std::cout << "Podaj pierwszy bok prostokąta: ";
+	std::cin >> firstSide;
+	std::cout << "Podaj drugi bok prostokąta: ";
+	std::cin >> secondSide;
+	area = firstSide * secondSide;
+	std::cout << "Pole prostokąta wynosi " << area << " cm2";
 
 
 }
 
 void task4()
 {
-    //*.Program obliczający objętość stożka.
-    double h, r, V ;
-    
-    std::cout << "Podaj wysokość stożka: ";
-    std::cin >> h;
-    std::cout << "Podaj promień koła w stożku: ";
-    std::cin >> r;
-    V = 1 / 3.0 * 3.14 * (r * r) * h;
-    std::cout << "Objętość stożka wynosi: " << V << " cm3";
+	//*.Program obliczający objętość stożka.
+	double high, r, V;
+
+	std::cout << "Podaj wysokość stożka: ";
+	std::cin >> high;
+	std::cout << "Podaj promień koła w stożku: ";
+	std::cin >> r;
+	V = 1 / 3.0 * 3.14 * (r * r) * high;
+	std::cout << "Objętość stożka wynosi: " << V << " cm3";
 
 }
 void task5()
 {
-    //*.Program obliczający pole koła.
-    double area, promien;
-    std::cout << "Podaj promień koła: ";
-    std::cin >> promien;
-    area = 3.14 * (promien * promien);
-    std::cout << "Pole koła wynosi: " << area << " cm2";
+	//*.Program obliczający pole koła.
+	double area, promien;
+	std::cout << "Podaj promień koła: ";
+	std::cin >> promien;
+	area = 3.14 * (promien * promien);
+	std::cout << "Pole koła wynosi: " << area << " cm2";
 }
 
 void task6()
 {
-    //*.Program obliczający wartość wyrażenia a ^ 2 + b ^ 2
-    int a, b, result;
-    std::cout << "podaj liczbe a: ";
-    std::cin >> a;
-    std::cout << "podaj liczbe a: ";
-    std::cin >> b;
-    result = (a + b) * (a + b);
-    std::cout << "Wynik to " << result;
+	//*.Program obliczający wartość wyrażenia a ^ 2 + b ^ 2
+	int a, b, result;
+	std::cout << "podaj liczbe a: ";
+	std::cin >> a;
+	std::cout << "podaj liczbe a: ";
+	std::cin >> b;
+	result = (a + b) * (a + b);
+	std::cout << "Wynik to " << result;
 }
 
 void task7()
 {
-    //*.Program obliczający pole trójkąta o podstawie b i wysokości h
+	//*.Program obliczający pole trójkąta o podstawie b i wysokości h
 
-    double base, high, area;
-    std::cout << "Podaj wysokość: ";
-    std::cin >> high;
-    std::cout << "Podaj podstawe: ";
-    std::cin >> base;
-    area = high * base / 2;
-    std::cout << "Pole trójkąta wynosi: " << area;
+	double base, high, area;
+	std::cout << "Podaj wysokość: ";
+	std::cin >> high;
+	std::cout << "Podaj podstawe: ";
+	std::cin >> base;
+	area = high * base / 2;
+	std::cout << "Pole trójkąta wynosi: " << area;
+
+}
+
+
+void task8()
+{
+	/*.Program obliczający objętość kuli o promieniu r*/
+	double size, ray;
+	std::cout << "Podaj promień: ";
+	std::cin >> ray;
+	size = 4 / 3.0 * (3.14 * (ray * ray * ray));
+	std::cout << "Wynik to: " << size << " cm3";
+
+
+
+}
+
+void task9()
+{
+	/*.Program obliczający pole trapezu o podstawach a i b oraz wysokości h*/
+	double firstBase, secondBase, high, radius;
+	std::cout << "Podaj pierwszą podstawe: ";
+	std::cin >> firstBase;
+	std::cout << "Podaj drugą podstawe: ";
+	std::cin >> secondBase;
+	std::cout << "Podaj wysokość: ";
+	std::cin >> high;
+	radius = ((firstBase + secondBase) * high) / 2.0;
+	std::cout << "Pole trapezu wynosi: " << radius << " cm2";
+
+
+
+
+}
+
+void task10()
+{
+	/*.Program obliczający średnią ważoną trzech liczb z podanymi odpowiednimi wagami w1, w2 i w3.*/
+	double firstRate, secondRate, thirdRate, firstWeight, secondWeight, thirdWeight, weightedAverage;
+	std::cout << "Podaj pierwszą ocenę: ";
+	std::cin >> firstRate;
+	std::cout << "Podaj jej wagę: ";
+	std::cin >> firstWeight;
+	std::cout << "Podaj drugą ocenę: ";
+	std::cin >> secondRate;
+	std::cout << "Podaj jej wagę: ";
+	std::cin >> secondWeight;
+	std::cout << "Podaj trzecią ocenę: ";
+	std::cin >> thirdRate;
+	std::cout << "Podaj jej wagę: ";
+	std::cin >> thirdWeight;
+	weightedAverage = ((firstRate * firstWeight) + (secondRate * secondWeight) + (thirdRate * thirdWeight)) / (firstWeight + secondWeight + thirdWeight);
+	std::cout << "Wynik to: " << weightedAverage;
+
+
+
 
 }
 int main()
 {
-    setlocale(LC_CTYPE, "polish");
-    //task1();
-    /*task2();*/
-    /*task3();*/
-    /*task4();*/
-    /*task5();*/
-    //task6();
-    task7();
+	setlocale(LC_CTYPE, "polish");
+	//task1();
+	/*task2();*/
+	/*task3();*/
+	/*task4();*/
+	/*task5();*/
+	//task6();
+	//task7();
+	/*task8();*/
+	/*task9();*/
+	task10();
+
+
 }
 
 //
