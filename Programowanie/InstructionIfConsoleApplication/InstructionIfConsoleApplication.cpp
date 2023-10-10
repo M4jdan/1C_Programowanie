b9f2752d7cd656b31cbe6f6c08a1f1ccd495478b
NIGGER <alanKYS@KYS.xyz>
2023-10-10 09:52:15 +0200

    if

---
+++ Programowanie/InstructionIfConsoleApplication/InstructionIfConsoleApplication.cpp
@@ -0,0 +1,23 @@
+﻿
+
+
+#include <iostream>
+
+void task1()
+{
+    int numberFromUser;
+    std::cout << "Podaj liczbę: ";
+    std::cin >> numberFromUser;
+    if (numberFromUser > 0)
+        std::cout << "Twoja liczba jest dodatnia";
+    else
+        std::cout << "Twoja liczba jest ujemna";
+}
+
+int main()
+{
+
+    task1();
+}
+
+
