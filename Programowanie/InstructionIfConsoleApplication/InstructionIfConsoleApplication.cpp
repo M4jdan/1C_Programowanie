164826aba7cfb592211a35ef7442676215c0e338
NIGGER <alanKYS@KYS.xyz>
2023-10-16 16:01:44 +0200

    if else

--- Programowanie/InstructionIfConsoleApplication/InstructionIfConsoleApplication.cpp
+++ Programowanie/InstructionIfConsoleApplication/InstructionIfConsoleApplication.cpp
@@ -10,14 +10,30 @@ void task1()
     std::cin >> numberFromUser;
     if (numberFromUser > 0)
         std::cout << "Twoja liczba jest dodatnia";
+    if (numberFromUser == 0)
+        std::cout << "Twoja liczba to 0";
     else
         std::cout << "Twoja liczba jest ujemna";
+}
+
+void task2()
+{
+    int numberFromUser;
+    std::cout << "Podaj liczbe: ";
+    std::cin >> numberFromUser;
+    if ((numberFromUser % 2) == 0)
+        std::cout << "Twoja liczba jest parzysta";
+    else
+        std::cout << "Twoja liczba jest nieparzysta";
+    
+
 }
 
 int main()
 {
 
-    task1();
+    //task1();
+    task2();
 }
 
 
