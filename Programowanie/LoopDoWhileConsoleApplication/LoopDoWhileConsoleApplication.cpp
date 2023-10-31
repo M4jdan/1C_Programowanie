860b09f4e1bf0b3c7ad531758ab74ee0d0033f50
NIGGER <alanKYS@KYS.xyz>
2023-10-31 09:04:04 +0100

    Pętla :)

--- Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
+++ Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
@@ -2,7 +2,32 @@
 
 void task1()
 {
+	int numberFromUser;
+	std::cout << "Podaj liczbę doodatnią:\n";
+	std::cin >> numberFromUser;
+	if (numberFromUser < 0)
+	{
+		std::cout << "Podaj liczbę doodatnią:\n";
+		std::cin >> numberFromUser;
+		if (numberFromUser < 0)
+		{
+			std::cout << "Podaj liczbę doodatnią:\n";
+			std::cin >> numberFromUser;
+			if (numberFromUser < 0)
+			{
+				std::cout << "Podaj liczbę doodatnią:\n";
+				std::cin >> numberFromUser;
+				if (numberFromUser < 0)
+				{
+					std::cout << "Podaj liczbę doodatnią:\n";
+					std::cin >> numberFromUser;
+					//wklejamy całego If'a
+				}
+			}
+		}
+	}
 
+	std::cout << "Liczba dodatnia pobrana od uzytkownika " << numberFromUser << "\n";
 }
 
 int main()
