9423d266b4fe3aa7c277777cbf6a3c6ccfe9f8b1
NIGGER <alanKYS@KYS.xyz>
2023-11-07 09:54:26 +0100

    lotto

--- Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
+++ Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
@@ -33,16 +33,43 @@ void task1()
 void task2()
 {
 	//lotto
-	int number = rand();
-
+	const int LOWER_RANGE = 1;
+	const int UPPER_RANGE = 100;
+	srand(time(NULL));
+	int randomNumber = rand() % (UPPER_RANGE - LOWER_RANGE + 1) + LOWER_RANGE;
+	int guessedNumber;
 	do
 	{
+		std::cout << "Podaj liczbe: \n";
+		std::cin >> guessedNumber;
 
-	}
-	while
+		if (guessedNumber > randomNumber)
+			std::cout << "Mniej\n";
+		if (guessedNumber < randomNumber)
+			std::cout << "Więcej\n";
+
+	} while (guessedNumber != randomNumber);
+
+	std::cout << "Gratulacje użytkowniku wygrałeś nic :DDD";
 }
 
+void task3()
+{
+	unsigned long long upperRange;
+	std::cout << "Podaj góre: \n";
+	std::cin >> upperRange;
+	
+	unsigned long long currentNumber = 0;
+	do
+	{
+		currentNumber = currentNumber + 1;
+		std::cout << currentNumber << ", ";
+
+	} while (upperRange > currentNumber);
+
+}
 int main()
 {
-    
+	setlocale(LC_CTYPE, "Polish");
+	task3();
 }
