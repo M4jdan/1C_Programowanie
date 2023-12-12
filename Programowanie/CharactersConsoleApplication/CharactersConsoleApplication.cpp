b8a50b162e7b3f8107e89d1d4200d451ade8fa12
NIGGER <alanKYS@KYS.xyz>
2023-12-12 09:48:02 +0100

    znaki

---
+++ Programowanie/CharactersConsoleApplication/CharactersConsoleApplication.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+void task1()
+{
+    char characterFromUser;
+    std::cout << "Podaj znak \n";
+    std::cin >> characterFromUser;
+
+    std::cout << "Podałeś znak : " << characterFromUser << "\n";
+}
+
+void task2()
+{
+    unsigned char characterFromUser;
+    std::cout << "Podaj znak \n";
+    std::cin >> characterFromUser;
+
+    if (characterFromUser >= 'a' && characterFromUser <= 'z')
+        std::cout << "Jest to mała litera alfabetu \n";
+    else 
+        std::cout << "Nie jest to mała litera alfabetu \n"
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "Polish");
+    task1();
+}
+
+
+/*
+Typy znakowe
+*char - 1 bajt całkowitoliczbowa ze znakiem <-128; 127>
+* 
+
+*/
--- Programowanie/LoopForConsoleApplication/LoopForConsoleApplication.cpp
+++ Programowanie/LoopForConsoleApplication/LoopForConsoleApplication.cpp
@@ -9,14 +9,10 @@
 
 * 
 * 
-* Program wyświetlający na ekranie wszystkie liczby podzielne przez 3 z zakresu od 1 do 100
-* Program obliczający sumę kwadratów liczb od 1 do 10
-* Program wyświetlający na ekranie ciąg Fibonacciego do 20 elementu (ciąg Fibonacciego to ciąg gdzie każdy element jest sumą dwóch poprzednich, np. 0, 1, 1, 2, 3, 5, 8, 13 itd.)
-* Program, kóry wyświetli poniższe cztery zwory:
-	****       54321        121212        122333
-	***        65432        212121        223334444
-	**         76543        121212        333444455555
-	*          87654        212121        444455555666666
+* 
+* 
+* 
+* 
 * Oblicz sumę szeregu 1/1^2 + 1/2^2 + 1/3^2 + ... +1/n^2. Jeżeli się nie pomylisz, to dla odpowiednio dużej wartości n po przemnożeniu jej przez 6 i spierwiastkowaniu powinieneś otrzymać wartość liczby π (suma szeregu jest równa π2/6). Zwróć uwagę, że pierwsza wartość indeksu to 1, a nie 0. Dokładność obliczeń można sprawdzić porównując z wartością odczytaną z własności Math.PI.
 * Rozkład liczby na czynniki pierwsze
 */
@@ -124,14 +120,90 @@ void task7()
 	}
 }
 
+//Program wyświetlający na ekranie tabliczkę mnożenia od 1 do 9 (np. 1x1=1, 1x2=2, 1x3=3 itd.).
 void task8()
 {
-	//Program wyświetlający na ekranie tabliczkę mnożenia od 1 do 9 (np. 1x1 = 1, 1x2 = 2, 1x3 = 3 itd.).
-	for (int i, k; ;);
+	//0x0	0x1		0x2		0x3	
+	//1x0	1x1		1x2		1x3	
+	//2x0	2x1		2x2		2x3	
+
+	for (int x = 0; x < 10; x++)
+	{
+		for (int y = 0; y < 10; y++)
+		{
+			std::cout << x << "x" << y << "=" << x * y << "\t";
+		}
+		std::cout << "\n";
+	}
 }
+
+void task9()
+{
+	//Program wyświetlający na ekranie wszystkie liczby podzielne przez 3 z zakresu od 1 do 100
+
+	for (int i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0)
+		{
+			std::cout << i << "\n";
+		}
+	}
+}
+
+void task10()
+{
+	//Program obliczający sumę kwadratów liczb od 1 do 10
+	int squareOfNumbers = 0;
+	for (int i = 1; i < 11; i++)
+	{
+		squareOfNumbers = i * i + squareOfNumbers;
+	}
+	std::cout << squareOfNumbers;
+}
+
+//Program wyświetlający na ekranie ciąg Fibonacciego do 20 elementu (ciąg Fibonacciego to ciąg gdzie każdy element jest sumą dwóch poprzednich, np. 0, 1, 1, 2, 3, 5, 8, 13 itd.)
+void task11()
+{
+	int f2 = 0;
+	int f1 = 1;
+	std::cout << f2 << ", " << f1 << ", ";
+	for (int i = 2; i < 20; i++)
+	{
+		int f = f1 + f2;
+		std::cout << f << ", ";
+		f2 = f1;
+		f1 = f;
+	}
+}
+
+void task12()
+{
+	/*Program, kóry wyświetli poniższe cztery zwory :
+	****54321        121212        122333
+		* **65432        212121        223334444
+		* *76543        121212        333444455555
+		* 87654        212121        444455555666666*/
+	const int NUMBER_OF_LINES = 4;
+	for (int lineNumber = 0; lineNumber < NUMBER_OF_LINES; lineNumber++)
+	{
+		for (int i = 0; i < NUMBER_OF_LINES - lineNumber; i++)
+			std::cout << "*";
+
+		std::cout << "\n";
+	}
+
+	for (int lineNumber = 0; lineNumber < NUMBER_OF_LINES; lineNumber++)
+	{
+		for (int i = 5; i > 0; i--)
+			std::cout << i;
+
+		std::cout << "\n";
+	}
+}
+
 int main()
 {
-	task7();
+	task12();
 }
 
 
