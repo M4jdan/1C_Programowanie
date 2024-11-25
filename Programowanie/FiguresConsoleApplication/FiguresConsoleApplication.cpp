d0f67b00c438b82704e353eb41e329ce1a930ed6
q <q>
2024-11-25 09:55:00 +0100

    figury

---
+++ Programowanie/FiguresConsoleApplication/FiguresConsoleApplication.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include "Figures.h"
+
+//Napisz klasy które opisz¹ figury geometryczne: 
+// trapez, romb, równoleg³obok, prostok¹t i kwadrat.
+// Zdefiniuj podstawowe metody pole obwód.
+
+int main()
+{
+    
+}
