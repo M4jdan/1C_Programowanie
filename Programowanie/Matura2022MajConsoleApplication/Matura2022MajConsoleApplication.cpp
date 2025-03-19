f661bb23cfaf633485ec3fb7dff0890d9a390669
q <q>
2025-03-19 13:17:54 +0100

    zadanie

--- Programowanie/Matura2022MajConsoleApplication/Matura2022MajConsoleApplication.cpp
+++ Programowanie/Matura2022MajConsoleApplication/Matura2022MajConsoleApplication.cpp
@@ -1,8 +1,47 @@
 // Matura2022MajConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 int main()
 {
-    
+    std::ifstream file("liczby.txt");
+
+    /*
+    for (int i = 0; i < 200; i++)
+    {
+        file >> numbers[i];
+    }
+
+    std::cout << "Odczytane liczby:\n";
+    for (int i = 0; i < 200; i++)
+    {
+        std::cout << numbers[i] << ", ";
+    }
+    */
+
+    std::vector<int> numbers;
+
+    int num;
+    while (file >> num)
+        numbers.push_back(num);
+
+    for (int num : numbers)
+    {
+        std::cout << num << ", ";
+    }
+
+    int count = 0;
+    for (int num : numbers)
+    {
+        int firstDigit;
+        int lastDigit = num % 10;
+
+        if (firstDigit == lastDigit)
+        {
+            count++;
+        }
+    }
+
 }
 
