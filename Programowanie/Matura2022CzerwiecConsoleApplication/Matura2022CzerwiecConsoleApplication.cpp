79726359ce12e937c53d4c42bec36e619330f5b8
q <q>
2025-04-09 13:19:05 +0200

    4.2

--- Programowanie/Matura2022CzerwiecConsoleApplication/Matura2022CzerwiecConsoleApplication.cpp
+++ Programowanie/Matura2022CzerwiecConsoleApplication/Matura2022CzerwiecConsoleApplication.cpp
@@ -2,37 +2,72 @@
 #include <vector>
 #include <fstream>
 
+std::vector<int> digits;
+int reflected_number;
+
+int reflectNumber(int n)
+{
+	int r_n = 0;
+	digits.clear();
+	do
+	{
+		digits.push_back(n % 10);
+		n = n / 10;
+
+	} while (n >= 1);
+	
+	for (int i = digits.size(); i > 0; i--)
+	{
+		r_n = r_n * 10 + *digits.begin();
+		digits.erase(digits.begin());
+
+	}
+
+	return r_n;
+}
+
 int main()
 {
 	std::ifstream file("przyklad.txt");
 
 	std::vector<int> numbers;
 
+	
+
 	int num;
 	while (file >> num)
 		numbers.push_back(num);
 
+	std::cout << "Zad 4.1 \n";
 	
-	std::vector<int> digits;
 	for (int num : numbers)
 	{
-		int reflected_number = 0;
-		digits.clear();
-		do 
-		{
-			digits.push_back(num % 10);
-			num = num / 10;
-
-		} while (num > 1);
-
-		for (int i = digits.size(); i >= 0 ; i--)
-		{
-			reflected_number += digits.begin() * pow(10, i);
-			digits.erase(digits.begin());
-		}
+		reflected_number = reflectNumber(num);
 
 		if (reflected_number % 17 == 0)
-			std::cout << reflected_number;
+			std::cout << reflected_number << "\n";
+
 	}
+
+	std::cout << "Zad 4.2 \n";
+
+	int n = 0;
+	int difference;
+
+	for (int num : numbers)
+	{
+		reflected_number = reflectNumber(num);
+
+		difference = num - reflected_number;
+		
+		if (difference < 0)
+			difference = difference * -1;
+
+		if (num > n)
+			n = num;
+
+	}
+	std::cout << n << "\n";
+	std::cout << difference;
 }
 
