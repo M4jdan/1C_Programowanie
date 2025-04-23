b30ff15d8404578755b822c4732924d41dc1bd55
q <q>
2025-04-23 13:02:38 +0200

    zadanuia

--- Programowanie/Matura2024MajConsoleApplication/Matura2024MajConsoleApplication.cpp
+++ Programowanie/Matura2024MajConsoleApplication/Matura2024MajConsoleApplication.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
+int ifOddAbbreviation(int n) 
+{
+	int m; 
+	
+	
+	return m;
+	
+	return 0;
+}
+
+int odd_num_abbreviation;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::ifstream file("skrot_przyklad.txt");
+	std::vector<int> numbers;
+
+	int num;
+	while (file >> num)
+		numbers.push_back(num);
+
+	std::cout << "Zad 3.2" << "\n";
+
+	int max_number = 0;
+	int count = 0;
+	for (int num : numbers)
+	{
+		odd_num_abbreviation = ifOddAbbreviation(num);
+		
+		if (odd_num_abbreviation == 0)
+		{
+			count++;
+			if (num > max_number)
+				max_number = num;
+		}
+
+	}
+
+	std::cout << count << "\n";
+	std::cout << max_number << "\n";
+
 }
 
 
