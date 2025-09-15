ac129254a0cbfb984ef17ae1ce4f1c2021ff2235
Sala213 <Sala213@ezse.pl>
2025-09-15 13:17:59 +0200

    ref out

--- Programowanie/FirstConsoleApp/Program.cs
+++ Programowanie/FirstConsoleApp/Program.cs
@@ -45,3 +45,23 @@ firstNumber = 15;
 Console.WriteLine($"firstNumber przed {firstNumber}");
 Parametr_v2(ref firstNumber);
 Console.WriteLine($"firstNumber po {firstNumber}");
+
+
+void Parametr_v3(out int p)
+{
+    //Console.WriteLine($"parametr 2 {}");
+    p=15;
+    Console.WriteLine($"Parametr 2 {p}");
+}
+
+int thirdNumber;
+Parametr_v3(out thirdNumber);
+Console.WriteLine($"firstNumber po {thirdNumber}");
+
+string firstStrNumber = "15";
+
+int firstConvertedNumber = int.Parse(firstStrNumber);
+Console.WriteLine($"Po konwersji {firstConvertedNumber}");
+
+if (int.TryParse(firstStrNumber, out int secondConvertedNumber))
+    Console.WriteLine($"Udało się skonwertować {secondConvertedNumber}");
