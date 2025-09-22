67fd8935d24270eef7d80858652185b59d6d1c11
Sala213 <Sala213@ezse.pl>
2025-09-22 13:13:33 +0200

    cos

---
+++ Programowanie/AboutObjectConsoleApp/Point.cs
@@ -0,0 +1,32 @@
+﻿
+namespace AboutObjectConsoleApp
+{
+    internal class Point
+    {
+        private int x;
+        private int y;
+
+
+        public Point()
+        {
+            x = 8;
+            y = 5;
+        }
+
+        public Point(int a, int b)
+        {
+            x = a;
+            y = b;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"({x}, {y})");
+        }
+
+        public void SetX(int x)
+        {
+            this.x = x;
+        }
+    }
+}
