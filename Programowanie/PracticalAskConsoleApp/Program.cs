8d6406db61c24c4f317c52b84449d75396919f14
Sala213 <Sala213@ezse.pl>
2025-10-16 16:50:12 +0200

    zadanie 2

--- Programowanie/PracticalAskConsoleApp/Program.cs
+++ Programowanie/PracticalAskConsoleApp/Program.cs
@@ -2,3 +2,13 @@
 task1 task1 = new task1();
 task1.task();
 
+
+Notatka notatka1 = new Notatka("zakupy", "masło szynka ser bułki");
+notatka1.viewNote();
+notatka1.diagnose();
+
+Notatka notatka2 = new Notatka("rzeczy", "stół krzesło lubnie placki");
+notatka2.viewNote();
+notatka2.diagnose(); 
+
+notatka1.diagnose();
--- Programowanie/PracticalAskConsoleApp/Styczeń 2023_1/notatka.cs
+++ Programowanie/PracticalAskConsoleApp/Styczeń 2023_1/notatka.cs
@@ -3,11 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace PracticalAskConsoleApp.Styczeń_2023_1
 {
-    internal class notatka
+
+    /*
+     ***********************************************
+     Klasa:     Note
+
+     Opis:      tworzenie notatek 
+
+     pola:      notes_count - licznik notatek
+                notes_id - identyfikator notatki
+                notes_title - tytuł notatki
+                notes_content - treść notatki
+     ****************************************************
+     */
+    internal class Notatka
     {
+        static private int notes_count = 0;
+        private int note_id;
+
+        protected string note_title;
+        protected string note_content;
+
+        public Notatka(string title, string content)
+        {
+
+            notes_count += 1;
+            note_id = notes_count;
+
+            note_title = title;
+            note_content = content;
+
+        }
+
+        public void viewNote()
+        {
+            Console.WriteLine($"Tytuł: {note_title} \n");
+            Console.WriteLine($"Treść: {note_content} \n");
+        }
+
+        public void diagnose()
+        {
+            Console.WriteLine($"Liczba utworzonych notatek : {notes_count}; Numer ID: {note_id}; Tytuł: {note_title}; Treść: {note_content} \n");
+        }
 
     }
 }
