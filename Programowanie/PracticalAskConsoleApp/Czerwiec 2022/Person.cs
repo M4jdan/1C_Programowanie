74dea357c3d97e02d3b68ff7b19b4138570669ba
Sala213 <Sala213@ezse.pl>
2025-10-20 13:19:21 +0200

    czerwiec 2022

---
+++ Programowanie/PracticalAskConsoleApp/Czerwiec 2022/Person.cs
@@ -0,0 +1,40 @@
+﻿
+namespace PracticalAskConsoleApp.Czerwiec_2022
+{
+    internal class Person
+    {
+        private int id;
+        private string name;
+
+        public int number_of_instance = 0;
+
+        public Person()
+        {
+            id = 0;
+            name = "";
+            number_of_instance++;
+        }
+
+        public Person(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+            number_of_instance++;
+        }
+
+        public Person(Person person)
+        {
+            this.id = person.id;
+            this.name = person.name;
+            number_of_instance++;
+        }
+
+        public void WriteName(string n)
+        {
+            if (this.name != "")
+                Console.WriteLine($"Cześć {n}, mam na imię {name}");
+            else
+                Console.WriteLine($"Cześć {n}, mam na imię 'Brak Danych' ");
+        }
+    }
+}
--- Programowanie/PracticalAskConsoleApp/Program.cs
+++ Programowanie/PracticalAskConsoleApp/Program.cs
@@ -1,8 +1,18 @@
-﻿using PracticalAskConsoleApp.Styczeń_2023_1;
+﻿using PracticalAskConsoleApp.Czerwiec_2022;
+using PracticalAskConsoleApp.Styczeń_2023_1;
+
+
+//Styczeń 2023_1
+
+/*
 task1 task1 = new task1();
 task1.task();
 
+*/
 
+//Styczeń 2023_2
+
+/*
 Note notatka1 = new Note("zakupy", "masło szynka ser bułki");
 notatka1.ViewNote();
 notatka1.Diagnose();
@@ -12,3 +22,18 @@ notatka2.ViewNote();
 notatka2.Diagnose(); 
 
 notatka1.Diagnose();
+
+*/
+
+//Czerwiec 2022 
+
+/*
+Person person = new Person(5, "Ktoś");
+person.WriteName("czesio");
+
+Person person2 = new Person(person);
+person.WriteName("cos");
+
+*/
+
+//
