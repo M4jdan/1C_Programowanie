35380f681400a7f7e5cde182e30099e7bf874687
Sala213 <Sala213@ezse.pl>
2026-02-12 12:26:03 +0100

    zadania

--- Programowanie/LinqPractocalTasksConsoleApp/Task1.cs
+++ Programowanie/LinqPractocalTasksConsoleApp/Task1.cs
@@ -9,20 +9,7 @@ namespace LinqPractocalTasksConsoleApp
 
     /*
 
-    ========================================
-    == POZIOM 2 – ŚREDNI ==
-    ========================================
-
-    11. Posortuj osoby po mieście, a w ramach miasta po pensji malejąco.
-    12. Znajdź osoby w wieku od 25 do 35 lat (włącznie).
-    13. Oblicz sumę pensji osób z Kielc.
-    14. Znajdź pierwszą osobę, której pensja jest większa niż 10 000.
-    15. Znajdź ostatnią osobę w kolejności alfabetycznej po nazwisku.
-    16. Wygeneruj dane osób w formacie: "Imię Nazwisko (Miasto)".
-    17. Sprawdź, czy wszyscy mają co najmniej 18 lat.
-    18. Policz, ile jest kobiet.
-    19. Znajdź osoby, które zarabiają więcej niż średnia pensja.
-    20. Znajdź najstarszą osobę z Krakowa.
+    
 
 
     ========================================
@@ -72,24 +59,9 @@ namespace LinqPractocalTasksConsoleApp
             new() { Id=7, FirstName="Iga",   LastName="Kowal",     Age=31, Gender=Gender.Female, City="Kraków", Salary=9900m,  Skills=["Go", "Kubernetes"] },
             new() { Id=8, FirstName="Tomek", LastName="Sikora",    Age=29, Gender=Gender.Male,   City="Kielce", Salary=8800m,  Skills=["C#", "MAUI", "Bluetooth"] },
         };
+
+
             /*
-            ========================================
-            == POZIOM 1 – PODSTAWOWY ==
-            ========================================
-
-            1. Znajdź wszystkie osoby z Krakowa.
-            2. Znajdź osoby pełnoletnie.
-            3. Posortuj osoby po wieku rosnąco.
-            4. Posortuj osoby po nazwisku malejąco.
-            5. Wypisz tylko imię i nazwisko każdej osoby.
-            6. Wypisz tylko miasta bez duplikatów.
-            7. Oblicz liczbę osób z Warszawy.
-            8. Oblicz średnią pensję wszystkich osób.
-            9. Znajdź najmłodszą osobę.
-            10. Sprawdź, czy jest ktoś z Gdańska.
-            */
-
-
             // Zadanie 1
             var q1 = people.Where(p => p.City == "Kraków");
             Print("Zadanie 1", q1);
@@ -105,15 +77,123 @@ namespace LinqPractocalTasksConsoleApp
 
             // zad 4
             var q4 = people.OrderByDescending(p => p.LastName);
+            Print("zad 4", q4);
 
             // zad 5
             IEnumerable<BoxFirstAndLastName> q5 = people.Select(p => new BoxFirstAndLastName() { FirstName = p.FirstName, LastName = p.LastName });
+            Print("zad 5", q5);
 
-            class BoxFirstAndLastName
-            {
+            //zad 6
+            var q6 = people.DistinctBy(p => p.City).Select(p => p.City);
+            Print("zad 6", q6);
+
+            // zad 7
+            var q7 = people.Where(p => p.City == "Warszawa").Count().ToString();
+            Print("zad 7", q7);
+
+            //zad 8
+            var q8 = people.Average(p => p.Salary);
+            Console.Write($"\n=== zad 8 ===  \n{q8}\n");
+
+            var q9 = people.OrderBy(p => p.Age).FirstOrDefault();
+            Console.Write($"\n=== zad 9 ===  \n{q9}\n");
+
+            var q10 = people.Any(p => p.City == "Gdańsk");
+            Console.Write($"\n=== zad 10 ===  \n{q10}\n");
+
+            */
+
+
+
+            /*
+             ========================================
+             == POZIOM 2 – ŚREDNI ==
+             ========================================
+
+             11. Posortuj osoby po mieście, a w ramach miasta po pensji malejąco.
+             12. Znajdź osoby w wieku od 25 do 35 lat (włącznie).
+             13. Oblicz sumę pensji osób z Kielc.
+             14. Znajdź pierwszą osobę, której pensja jest większa niż 10 000.
+             15. Znajdź ostatnią osobę w kolejności alfabetycznej po nazwisku.
+             16. Wygeneruj dane osób w formacie: "Imię Nazwisko (Miasto)".
+             17. Sprawdź, czy wszyscy mają co najmniej 18 lat.
+             18. Policz, ile jest kobiet.
+             19. Znajdź osoby, które zarabiają więcej niż średnia pensja.
+             20. Znajdź najstarszą osobę z Krakowa.
+            */
+
+            /*
+            //zad 11
+            var q11 = people.OrderBy(p => p.City).OrderByDescending(p => p.Salary);
+            Print("zad 11", q11);
+
+            //zad 12
+            var q12 = people.Where(p => p.Age >= 25 && p.Age <= 35);
+            Print("zad 12", q12);
+
+            //zad 13
+            var q13 = people.Where(p => p.City == "Kielce").Sum(p => p.Salary);
+            Console.Write($"\n=== zad 13 ===  \n{q13}\n");
+
+            //zad 14
+            var q14 = people.Where(p => p.Salary > 10000).OrderBy(p => p.Salary).FirstOrDefault();
+            Console.Write($"\n=== zad 14 ===  \n{q14}\n");
+
+            //zad 15
+            var q15 = people.OrderBy(p => p.LastName).Last();
+            Console.Write($"\n=== zad 15 ===  \n{q15}\n");
+
+            //zad 16
+            var q16 = people.Select(p => new { Name = p.FirstName + " " + p.LastName, p.City});
+            Print("zad 16", q16);
+
+            //zad 17
+            var q17 = people.All(p => p.Age >= 18);
+            Console.Write($"\n=== zad 17 ===  \n{q17}\n");
+
+            //zad 18
+            var q18 = people.Count(p => p.Gender == Gender.Female);
+            Console.Write($"\n=== zad 18 ===  \n{q18}\n");
+
+            //zad 19
+            var avg_salary = people.Average(p => p.Salary);
+            var q19 = people.Where(p => p.Salary > avg_salary);
+            Print("zad 19", q19);
+
+            //zad 20
+            var q20 = people.Where(p => p.City == "Kraków").OrderBy(p => p.Age).LastOrDefault();
+            Console.Write($"\n=== zad 20 ===  \n{q20}\n");
+            */
+
+            /*
+            ========================================
+            == POZIOM 3 – ZAAWANSOWANY ==
+            ========================================
+
+            22. Znajdź osoby, które mają skill "C#".
+            23. Znajdź osoby, które mają co najmniej trzy skille.
+            24. Znajdź osoby z Warszawy, posortuj je po wieku malejąco i wypisz tylko ich imię, nazwisko, wiek i pensję.
+            25. Sprawdź, czy ktoś ma skill "Azure".
+            26. Sprawdź, czy wszyscy zarabiają co najmniej 4000.
+            27. Znajdź osobę o największej pensji.
+            28. Znajdź osobę o najmniejszej pensji.
+            29. Wypisz osoby, które mają taki sam wiek jak najstarsza osoba.
+             */
+
+
+        }
+
+        class BoxFirstAndLastName
+        {
             public string FirstName { get; init; }
 
             public string LastName { get; init; }
-            } 
+
+            public override string ToString()
+                => $"{FirstName}, {LastName}";
+
+
+        }
+
     }
 }
